78d6055347b7de86e848f2bf429efd9f5c2fa08e
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-01-17 17:04:46 -0300

    consolidated route para entity

--- src/route.entity.ts
+++ src/route.entity.ts
@@ -4,26 +4,21 @@ export type RouteProps = {
   title: string,
   startPosition: LatLng,
   endPosition: LatLng,
-  points: LatLng[]
+  points?: LatLng[]
 
 }
 
 export class Route {
-  constructor(public props: RouteProps) { }
+  public props: Required<RouteProps>
+  constructor(props: RouteProps) {
+    this.props = {
+      ...props,
+      points: props.points || []
+    }
+  }
 
   private set title(value: string) {
     this.props.title = value
 
   }
-
 }
-
-const rota = new Route({
-  title: 'minha rota',
-  startPosition: { lat: 15, lng: 15 },
-  endPosition: { lat: 20, lng: 20 },
-  points: [
-    { lat: 15, lng: 15 },
-    { lat: 15, lng: 15 },
-  ]
-})

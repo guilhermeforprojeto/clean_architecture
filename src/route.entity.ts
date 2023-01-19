40dd6be54ed12a86f1606d1427ceb783c6968dea
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-01-19 14:52:20 -0300

    Metados de atualizar Title e Position

--- src/route.entity.ts
+++ src/route.entity.ts
@@ -16,9 +16,45 @@ export class Route {
       points: props.points || []
     }
   }
-
+  updateTitle(title: string) {
+    this.title = title
+  }
+  // mudar para maiusculo
+  // valor alguns caracteres
+  // validações 
+  // Vulgo as regras de nedógio
+  updatePosition(startPosition: LatLng, endPosition: LatLng) {
+    this.startPosition = startPosition;
+    this.endPosition = endPosition;
+    // mudar para maiusculo
+    // valor alguns caracteres
+    // validações 
+    // Vulgo as regras de nedógio
+  }
+  get title() {
+    return this.props.title
+  }
   private set title(value: string) {
     this.props.title = value
 
   }
+
+  get startPosition() {
+    return this.props.startPosition
+  }
+  private set startPosition(value: LatLng) {
+    this.props.startPosition = value
+
+  }
+
+
+  get endPosition() {
+    return this.props.startPosition
+  }
+  private set endPosition(value: LatLng) {
+    this.props.startPosition = value
+
+  }
+
+
 }

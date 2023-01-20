5f7e983babcb889ca73983f95d23dcae8d158de8
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-01-20 18:03:27 -0300

    teste updatePoints

--- src/route.entity.spec.ts
+++ src/route.entity.spec.ts
@@ -47,5 +47,17 @@ describe('Route Tests', () => {
     expect(route.startPosition).toBe(startPosition)
     expect(route.endPosition).toBe(endPosition)
   })
+  test('updatePoints method', () => {
+    const routeProps: RouteProps = {
+      title: 'minha rota',
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+    }
+    const route = new Route(routeProps);
+    const points: LatLng[] = [{ lat: 10, lng: 20 }];
+    route.updatePoints(points);
+    expect(route.points).toHaveLength(1)
+    expect(route.points).toStrictEqual(points)
+  })
 
 })

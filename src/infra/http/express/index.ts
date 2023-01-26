cc232cc62546bfb8f27496a9319995f9cb74016c
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-01-26 17:26:39 -0300

    index do servidor express

--- src/infra/http/express/index.ts
+++ src/infra/http/express/index.ts
@@ -3,10 +3,21 @@ import { CreateRouteUseCase } from '../../../application/create_route.use_case'
 import { RouteInMemoryRepository } from '../../db/route_in_memory.repository'
 
 const app: Express = express()
+app.use(express.json())
+
+
+const port = process.env.PORT || 3000
 
 const routeRepo = new RouteInMemoryRepository();
+
 app.post('/routes', async (req: Request, res: Response) => {
   const createUseCase = new CreateRouteUseCase(routeRepo);
   const output = await createUseCase.execute(req.body);
   res.status(201).json(output)
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running port ${port}`);
+
 })

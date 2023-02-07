277110b71e35fdc97d28fa6fb843742214ffa795
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-02-07 16:57:16 -0300

    Add API REST NEST

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [],
+  imports: [RoutesModule],
   controllers: [AppController],
   providers: [AppService],
 })

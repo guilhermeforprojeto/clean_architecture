19ef5f3fa7b47f0338154c907b0fcce37554743d
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-02-08 17:10:38 -0300

    implementação de typeORM

---
+++ src/@core/infra/db/typeorm/route.schema.ts
@@ -0,0 +1,27 @@
+import { title } from "process";
+import { Entity, EntitySchema } from "typeorm";
+import { Route } from "../../../domain/route.entity";
+
+export const RouteSchema = new EntitySchema<Route>({
+  name: 'route',
+  target: Route,
+  columns: {
+    id: {
+      type: 'uuid',
+      primary: true
+    },
+    title: {
+      type: String,
+      length: 255
+    },
+    startPosition: {
+      type: 'simple-json'
+    },
+    endPosition: {
+      type: 'simple-json'
+    },
+    points: {
+      type: 'simple-json'
+    },
+  }
+})

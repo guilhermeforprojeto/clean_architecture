560d0e4cc755b460f4921a7f30588aac309d4116
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-02-10 16:27:01 -0300

    add import TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { RouteSchema } from './@core/infra/db/typeorm/route.schema';
+import { join } from 'path'
+
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(__dirname, 'database.sqlite'),
+      synchronize: true,
+      logging: true,
+      entities: [RouteSchema]
+    }),
+    RoutesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
